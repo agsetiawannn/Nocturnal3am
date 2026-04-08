85aacdda3189cdab6a25dbf3a7cf1174a9dcf12a
agsetiawannn <setiawan18221@gmail.com>
2026-04-08 19:28:32 +0800

    Fix: Use flex-col-reverse lg:flex-col for mobile/desktop video positioning on Home

--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -286,9 +286,9 @@ function Home() {
 
 
             {/* Responsive Wrapper to swap Video and Contact order on mobile/desktop */}
-            <div className="flex flex-col">
+            <div className="flex flex-col-reverse lg:flex-col">
                 {/* Motion Video Section */}
-                <section className="bg-black order-2 lg:order-1">
+                <section className="bg-black">
                     <video
                         src="/public/img/MOTION TP.MP4"
                         autoPlay
@@ -301,7 +301,7 @@ function Home() {
                 </section>
 
                 {/* Contact Section */}
-                <section className="relative bg-black order-1 lg:order-2 flex flex-col justify-center lg:min-h-[600px]">
+                <section className="relative bg-black flex flex-col justify-center lg:min-h-[600px]">
                     <div className="absolute inset-0">
                         <img
                             src="/public/img/inquiries.webp"
